be1d06600b5a8d16815fc1a26d2ef71ae45107b8
narang-v <84620915+narang-v@users.noreply.github.com>
2023-11-26 15:50:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# This file is the Frontend Application that creates a local host server in the machine.
+# The URL http://127.0.0.1:5000/ displays the query search engine.
+# You can here enter your query and the search results will be displayed in the tabular format
+# This file uses the llm_searcher functionality from "search_main.py" to generate results of the entered query.
 #Libraries import
 from flask import Flask, render_template, request, jsonify
 from search_main import LLMSearcher
